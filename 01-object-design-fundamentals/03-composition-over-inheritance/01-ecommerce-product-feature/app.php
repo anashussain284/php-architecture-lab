06d59cbae8efecd5d8221d4519b8f5af57397bbe
Anas Hussain M <anashussain284@gmail.com>
2026-07-27 09:14:36 +0530

    feat(coi): ecommer-product assembly on runtime
    Build different products on runtime use composition with multiple
    modules

--- 01-object-design-fundamentals/03-composition-over-inheritance/01-ecommerce-product-feature/app.php
+++ 01-object-design-fundamentals/03-composition-over-inheritance/01-ecommerce-product-feature/app.php
@@ -18,6 +18,10 @@ use App\Services\Wishlist\WishlistEnabled;
 use App\Services\Pricing\Features\CurrencyConverter;
 use App\Services\Pricing\Features\PriceFormatter;
 use App\Services\Pricing\Features\TaxCalculator;
+use App\Services\Pricing\PremiumPrice;
+use App\Services\Reviews\FeaturedReviews;
+use App\Services\Shipping\DigitalShipping;
+use App\Services\Shipping\FreeShipping;
 
 $money = new Money(
 	amountInCent: 10000,
@@ -53,3 +57,43 @@ $mobile = new Product(
 );
 $productPresenter = new ProductPresenter();
 $productPresenter->show(product: $mobile);
+
+$gamingKeyboard = new Product(
+	name: 'Gaming Keyboard',
+	pricing: $discountPrice,
+	inventory: $outOfStockInventory,
+	review: new FeaturedReviews(),
+	shipping: new FreeShipping(),
+	wishlist: $wishlistEnabled
+);
+
+$productPresenter->show(product: $gamingKeyboard);
+
+$ebook = new Product(
+	name: 'E-Book',
+	pricing: $fixedPrice,
+	inventory: $infiniteInventory,
+	review: new FeaturedReviews(),
+	shipping: new DigitalShipping(),
+	wishlist: $wishlistEnabled
+);
+
+$productPresenter->show(product: $ebook);
+
+$premiumPrice = new PremiumPrice(
+	amount: 50000,
+	currencyConverter: new CurrencyConverter(),
+	priceFormatter: new PriceFormatter(),
+	taxCalculator: new TaxCalculator()
+);
+
+$luxuryWatch = new Product(
+	name: 'Luxury Watch',
+	pricing: $premiumPrice,
+	inventory: $infiniteInventory,
+	review: $simpleReviews,
+	shipping: $paidShipping,
+	wishlist: $wishlistEnabled
+);
+
+$productPresenter->show(product: $luxuryWatch);
